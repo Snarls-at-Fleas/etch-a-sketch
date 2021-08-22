a74e12bf50f2ff91227ba81b313b9fe582e4fe75
Snarls-at-Fleas <berlin.pavel@gmail.com>
2021-08-22 06:44:46 +0300

    Add customizable canvas (16-100)

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-let containter = document.getElementById('sketchPad');
-for (i = 0; i <= 100; i++) {
-    for (j = 0; j <=100; j++) {
-        let pixel = document.createElement('div');
-        pixel.setAttribute('id', `${i}${j}`);
-        pixel.classList.toggle('cell');
-        containter.appendChild(pixel);
+// Add a set of radio buttons to select
+// Black, rainbow or shades of grey filling options
+
+function makeNewCanvas(canvSize) {
+    let containter = document.getElementById('sketchPad');
+    while (containter.firstChild) {
+        containter.removeChild(containter.lastChild);
+    };
+    console.log(canvSize);
+    for (i = 0; i <= canvSize-1; i++) {
+        for (j = 0; j <= canvSize-1; j++) {
+            let pixel = document.createElement('div');
+            pixel.setAttribute('id', `${i}${j}`);
+            pixel.classList.toggle('cell');
+            containter.appendChild(pixel);
+        }
     }
+    let pixels = document.querySelectorAll('.cell');
+    pixels.forEach(pixel => {
+        pixel.style.width = `${720/canvSize}px`;
+        pixel.style.height = `${720/canvSize}px`;
+    });
+    let canvasSizeHeader = document.querySelector('#canvasSize');
+    canvasSizeHeader.textContent = `Current canvas: ${canvSize}x${canvSize}`
 }
+
+makeNewCanvas(16);
 
 let cellHovered = document.querySelector('#sketchPad');
 cellHovered.addEventListener('mouseover', (e) => {
@@ -18,5 +36,14 @@ cellHovered.addEventListener('mouseover', (e) => {
 let btn = document.querySelector('button');
 btn.addEventListener('click', () => {
     let pixels = document.querySelectorAll('.cell');
-    pixels.forEach(pixel => pixel.style.backgroundColor = 'white'); 
+    pixels.forEach(pixel => pixel.style.backgroundColor = 'white');
+    let sizeCheck = true;
+    let canvasSize = 16;
+    while (sizeCheck) {
+        canvasSize = prompt("Enter new canvas size (16-100):");
+        if (canvasSize >= 16 && canvasSize <= 100) {
+            sizeCheck = false;
+        }
+    };
+    makeNewCanvas(canvasSize);
 })
